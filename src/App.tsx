aac8e1cfef6504b29d8e394d50e7ff99942a2410
David Sajitz <128414125+Dasajton@users.noreply.github.com>
2024-02-05 21:48:25 +0100

    David: Added Paragraph

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 function App() {
-
   return (
     <>
-    <h1 className=" text-2xl text-red-400">Currency Site</h1>
-    <p>Welcome to this cool site.</p>
 
-      
-    <h2>Edward's Section</h2>
-    <p>This is Edward's content.</p>
+      <h1 className="text-2xl text-red-400">Currency Site</h1>
+      <p>Welcome to this cool site.</p>
+      <p>Buy crypto and get right just overnight!!!</p>
+      <h2>Edward's Section</h2>
+      <p>This is Edward's content.</p>
+
     </>
-  )
+  );
 }
 
-export default App
+export default App;
