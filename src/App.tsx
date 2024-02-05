30c213dfac3772ed9dbbe2eaea9fc4a020f9b4c5
Dasajton <david.sajitz.94@gmail.com>
2024-02-05 21:41:57 +0100

    added paragraph

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 function App() {
-
   return (
     <>
-    <h1>Currency Site</h1>
-    <p>Welcome to this cool site.</p>
+      <h1>Currency Site</h1>
+      <p>Welcome to this cool site.</p>
+      <p>Buy crypto and get right just overnight!!!</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
