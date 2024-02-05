847d76d3bb0d4f45f881ccf6bc45c0c1a92e268c
Edward Tanguay <edward@tanguay.info>
2024-02-05 21:43:31 +0100

    Edward: added my own section

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,10 @@ function App() {
     <>
     <h1 className=" text-2xl text-red-400">Currency Site</h1>
     <p>Welcome to this cool site.</p>
+
+      
+    <h2>Edward's Section</h2>
+    <p>This is Edward's content.</p>
     </>
   )
 }
